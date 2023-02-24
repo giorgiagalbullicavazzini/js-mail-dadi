4904a96ca3dea736e6ac62c0f9082209169ce660
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-24 16:39:00 +0100

    wins counter

--- dice/js/main.js
+++ dice/js/main.js
@@ -1,6 +1,14 @@
 // Using a button, the user can play a dice game against the computer
 const play = document.getElementById('play');
 
+let win = 0;
+let lose = 0;
+let tie = 0;
+
+let winCount = document.getElementById('winner');
+let loseCount = document.getElementById('loser');
+let tieCount = document.getElementById('tie');
+
 // After a click on the button (`addEventListener`), the code generates two random numbers (one for the user and one for the computer) between 1 and 6 and saves them in two variables
 play.addEventListener('click',
   function() {
@@ -12,9 +20,15 @@ play.addEventListener('click',
     // ELSE the numbers are equal and the result is a tie
     if (playerDice > cpuDice) {
       alert(`Hai totalizzato ${playerDice} e il computer ha totalizzato ${cpuDice}. Hai vinto!`);
+      win += 1;
+      winCount.innerHTML = `${win}`;
     } else if (cpuDice > playerDice) {
       alert(`Hai totalizzato ${playerDice} e il computer ha totalizzato ${cpuDice}. Hai perso :(`);
+      lose += 1;
+      loseCount.innerHTML = `${lose}`;
     } else {
       alert(`Hai totalizzato ${playerDice} e il computer ha totalizzato ${cpuDice}. Pareggio... Riprova!`);
+      tie += 1;
+      tieCount.innerHTML = `${tie}`;
     }
 });
