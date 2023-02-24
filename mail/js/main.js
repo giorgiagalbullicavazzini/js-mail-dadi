e94ea5f0226afde07ee02bdbd2f9a87d5f082a0e
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-24 15:36:37 +0100

    mail inside the list check

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,7 +1,21 @@
+// The purpose of the code is to check if a given email is part of a specific list
+// The user can submit their email using an `input type email`
+const login = document.getElementById('login');
+
 // The code reads the entire list, provided with an `array`, by using a `for` loop
-// IF the user email is inside the list, the code produces a successful output
-// ELSE the code produces an error output
+const list = ['abc@gmail.com', 'def@gmail.com', 'ghi@gmail.com', 'jkl@gmail.com', 'mno@gmail.com'];
 
-const list = ["abc@gmail.com", "def@gmail.com", "ghi@gmail.com", "jkl@gmail.com", "mno@gmail.com"];
+login.addEventListener('click',
+  function () {
+    const email = document.getElementById('email').value;
 
-console.log(list);
+    // IF the user email is inside the list, the code produces a successful output
+    // ELSE the code produces an error output
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === email) {
+        console.log('Puoi accedere');
+      } else {
+        console.log('Riprova');
+      };
+    }
+  });
