1d12c72d6ce278cc36736172fb634415e035ad1d
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-24 16:08:19 +0100

    dice game

--- dice/js/main.js
+++ dice/js/main.js
@@ -1,5 +1,20 @@
 // Using a button, the user can play a dice game against the computer
+const play = document.getElementById('play');
+
 // After a click on the button (`addEventListener`), the code generates two random numbers (one for the user and one for the computer) between 1 and 6 and saves them in two variables
-// IF the user number is greater than the computer one, the user wins
-// ELSE IF the computer numbers is greater than the user one, the computer wins
-// ELSE the numbers are equal and the result is a tie
+play.addEventListener('click',
+  function() {
+    const playerDice = Math.floor(Math.random() * 6) + 1;
+    const cpuDice = Math.floor(Math.random() * 6) + 1;
+    
+    // IF the user number is greater than the computer one, the user wins
+    // ELSE IF the computer numbers is greater than the user one, the computer wins
+    // ELSE the numbers are equal and the result is a tie
+    if (playerDice > cpuDice) {
+      console.log(playerDice, cpuDice, 'Hai vinto');
+    } else if (cpuDice > playerDice) {
+      console.log(playerDice, cpuDice, 'Hai perso');
+    } else {
+      console.log(playerDice, cpuDice, 'Pareggio');
+    }
+});
