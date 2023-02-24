2f9f769dfce533ca8c106ab955fefed55d03e807
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-24 15:59:20 +0100

    dice html layout

---
+++ dice/js/main.js
@@ -0,0 +1,5 @@
+// Using a button, the user can play a dice game against the computer
+// After a click on the button (`addEventListener`), the code generates two random numbers (one for the user and one for the computer) between 1 and 6 and saves them in two variables
+// IF the user number is greater than the computer one, the user wins
+// ELSE IF the computer numbers is greater than the user one, the computer wins
+// ELSE the numbers are equal and the result is a tie
