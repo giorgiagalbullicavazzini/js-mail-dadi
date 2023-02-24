f8c9790b4e2e76f5a24823e9b96f3ea8f1c011f7
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-24 14:52:08 +0100

    mail html layout

---
+++ mail/js/main.js
@@ -0,0 +1,5 @@
+// The purpose of the code is to check if a given email is part of a specific list
+// The user can submit their email using an `input type email`
+// The code reads the entire list, provided with an `array`, by using a `for` loop
+// IF the user email is inside the list, the code produces a successful output
+// ELSE the code produces an error output
