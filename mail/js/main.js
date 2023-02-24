fff7a8195161be2a261c0b88c51878dceb5b208e
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-02-24 14:56:07 +0100

    mail array creation

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,5 +1,7 @@
-// The purpose of the code is to check if a given email is part of a specific list
-// The user can submit their email using an `input type email`
 // The code reads the entire list, provided with an `array`, by using a `for` loop
 // IF the user email is inside the list, the code produces a successful output
 // ELSE the code produces an error output
+
+const list = ["abc@gmail.com", "def@gmail.com", "ghi@gmail.com", "jkl@gmail.com", "mno@gmail.com"];
+
+console.log(list);
